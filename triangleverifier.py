77a337ef1886183e72f61a23990a4e0d09c15a50
Akshit1106 <akshit.kumar.mca.2025@miet.ac.in>
2026-02-19 14:38:25 +0530

    Initial commit

---
+++ triangleverifier.py
@@ -0,0 +1,9 @@
+angle1 = float(input("enter angle 1:"))
+angle2 = float(input("enter angle 2:"))
+angle3 = float(input("enter angle 3:"))
+# check condition
+
+if angle1 > 0 and angle2 > 0 and angle3 > 0 and ( angle1 + angle2 + angle3 == 180):
+    print("The angles form a triangle.")
+    else
+    print("The angle form a triangle.")
