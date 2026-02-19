49079fc65054dd2a40300e7ff75c4db41349e7c1
Akshit1106 <akshit.kumar.mca.2025@miet.ac.in>
2026-02-19 15:08:25 +0530

    changes

---
+++ shopingbill.py
@@ -0,0 +1,16 @@
+#price
+notebook_price = 45
+pen_price = 20
+#Quantities
+notebook = 3
+pens = 2
+#calculate total bill
+total_bill = (notebook * notebook_price) + (pens * pen_price)
+#customer payment
+payment = 500
+# calculate remaining balance
+remaining_balance = payment - total_bill
+#output
+print("Total bill Amount=", total_bill)
+print("Ramaining Balance =", remaining_balance)
+
--- triangleverifier.py
+++ triangleverifier.py
@@ -5,5 +5,5 @@ angle3 = float(input("enter angle 3:"))
 
 if angle1 > 0 and angle2 > 0 and angle3 > 0 and ( angle1 + angle2 + angle3 == 180):
     print("The angles form a triangle.")
-    else
+else:
     print("The angle form a triangle.")
